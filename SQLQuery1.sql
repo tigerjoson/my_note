f098ab045668321679b13c962a5323846271c706
tigerjoson <tigerjoson@gmail.com>
2018-06-16 17:58:22 +0800

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   Angular - NgModule API.txt
    	new file:   SQLQuery1.sql
    	new file:   devOps.png
    	new file:   generic.cs
    	new file:   git_command_note.txt
    	new file:   linux_apache_mysql_php.txt
    	new file:   log.txt
    	new file:   oh_my_zsh_note.txt
    	new file:   regex_with_sed.txt
    	new file:   win_note.txt
    	new file:   "\345\260\210\346\245\255\350\241\223\350\252\236.txt"

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+/* create database school_list
+use school_list
+go --pause
+create table [Su3](
+[¥N½X] int not null primary key
+[¹q¸Ü] 
+-- XXX-XXXX-XX
+)--create table & define
+--can not just create table
+go
+ */
+select *  from [dbo].[u1_new]
---
+++ generic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    //declare an generics
+    public class GList<T>
+    {
+    }
+
+    public class TestG
+    {
+        public static void Main()
+        {
+        }
+    }
+
+}
