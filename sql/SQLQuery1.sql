f244898a99486824218971188412c24535557ca4
tigerjoson <tigerjoson@gmail.com>
2018-07-03 13:11:06 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   sql/SQLQuery1.sql
    	modified:   "\350\263\207\347\256\241\351\233\234\350\253\207/\345\260\210\346\245\255\350\241\223\350\252\236.txt"

--- sql/SQLQuery1.sql
+++ sql/SQLQuery1.sql
@@ -1,22 +1,33 @@
-/* create database school_list
+/* 
+create database school_list
 use school_list
 go --pause
 create table [Su3](
 [代碼] int not null primary key
 [電話] 
 -- XXX-XXXX-XX
-)--create table & define
+)
+--create table & define
 --can not just create table
 go
  */
 
 	--列出所有資料表(Table)
- /*
 	USE YourDBName
 	GO 
 	SELECT *
 	FROM sys.Tables
 	GO 
- */
+
+--列出所有檢視表(View)
+SELECT *
+FROM sys.views
+GO 
+ --lookup schema
+ SELECT * 
+FROM sys.objects 
+-- find all objects in a SQL Server Schema
+WHERE schema_id = SCHEMA_ID('dbo')
+
  use [ELogisticDb]
 select *  from [dbo].[ChannelSettings]
